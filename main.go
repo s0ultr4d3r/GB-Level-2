d05638210660d7c7cd1429c4a316e72b160a5964
Anton Bondar <s0ul.tr4d3r@gmail.com>
2021-05-19 16:14:01 +0300

    lesson-1
    первые два задания

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+var list [5]string
+
+var (
+	ErrIndex = errors.New("oversized")
+)
+
+func panicHandler() {
+	if v := recover(); v != nil {
+		buff := make([]byte, 1024)
+		runtime.Stack(buff, false)
+		moment := time.Now()
+		fmt.Printf("Catched panic: %v, %s\n Time: %s\n", v, buff, moment)
+	}
+}
+func overloadList() {
+
+	for i := 0; i < 10; i++ {
+		list[i] = "elem №: " + strconv.Itoa(i)
+	}
+}
+
+func mixIt() {
+	defer panicHandler()
+	overloadList()
+}
+func main() {
+	mixIt()
+	fmt.Println(list)
+	fmt.Println("panic overcoming")
+}
